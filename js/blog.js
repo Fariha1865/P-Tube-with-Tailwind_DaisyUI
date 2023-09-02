8638abc55efa08622163d9c6d680b4d8518cb811
Fariha1865 <farihaamin187@gmail.com>
2023-09-02 10:59:38 +0600

    blog page modified

---
+++ js/blog.js
@@ -0,0 +1,21 @@
+document.getElementById('btn1').addEventListener('click', function () {
+
+    const modal = document.getElementById('my_modal');
+    modal.scrollTop = '0'; 
+    my_modal.showModal();
+    
+})
+document.getElementById('btn2').addEventListener('click', function () {
+
+    const modal = document.getElementById('my_modal_2');
+    modal.scrollTop = '0'; 
+    my_modal_2.showModal();
+    
+})
+document.getElementById('btn3').addEventListener('click', function () {
+
+    const modal = document.getElementById('my_modal_3');
+    modal.scrollTop = '0'; 
+    my_modal_3.showModal();
+    
+})
